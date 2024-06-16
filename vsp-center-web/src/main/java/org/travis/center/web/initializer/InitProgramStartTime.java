20043b4350bd3678f0a02d1c7a6dce34cb942239
travis <travis_x@163.com>
2024-06-16 10:43:44 +0800

    添加定时任务

---
+++ vsp-center-web/src/main/java/org/travis/center/web/initializer/InitProgramStartTime.java
@@ -0,0 +1,36 @@
+package org.travis.center.web.initializer;
+
+import cn.hutool.core.date.DateUtil;
+import com.github.benmanes.caffeine.cache.Cache;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+import org.travis.shared.common.constants.SystemConstant;
+
+import javax.annotation.Resource;
+
+/**
+ * @ClassName InitProgramStartTime
+ * @Description 初始化程序启动时间（本地缓存）
+ * @Author travis-wei
+ * @Version v1.0
+ * @Data 2024/6/15
+ */
+@Slf4j
+@Component
+@Order(Ordered.LOWEST_PRECEDENCE)
+public class InitProgramStartTime implements CommandLineRunner {
+
+    @Resource
+    public Cache<String, String> commonPermanentCache;
+
+    @Override
+    public void run(String... args) {
+        long timeMillis = System.currentTimeMillis();
+        log.info("[Eventual] Program started time:{} | {}", timeMillis, DateUtil.date(timeMillis));
+        commonPermanentCache.put(SystemConstant.PROGRAM_START_TIME_KEY, String.valueOf(timeMillis));
+        log.info("[Eventual] Program started time cache finished!");
+    }
+}
