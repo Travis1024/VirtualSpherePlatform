0396ad4bda5aba180cd123ce16474579ee8502be
travis <travis_x@163.com>
2024-06-24 23:04:12 +0800

    完善 Quartz 定时任务

--- vsp-center-support/src/main/java/org/travis/center/support/controller/ScheduleJobController.java
+++ vsp-center-support/src/main/java/org/travis/center/support/controller/ScheduleJobController.java
@@ -1,10 +1,15 @@
 package org.travis.center.support.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import org.travis.center.common.entity.support.ScheduleJob;
+import org.travis.center.common.enums.BusinessTypeEnum;
+import org.travis.center.support.aspect.Log;
 import org.travis.center.support.service.ScheduleJobService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
 * (VSP.VSP_SCHEDULE_JOB)表控制层
@@ -20,5 +25,11 @@ public class ScheduleJobController {
     private ScheduleJobService scheduleJobService;
 
 
+    @Log(title = "查询定时任务列表", businessType = BusinessTypeEnum.QUERY)
+    @Operation(summary = "查询定时任务列表")
+    @GetMapping("/list")
+    public List<ScheduleJob> selectList() {
+        return scheduleJobService.selectList();
+    }
 
 }
